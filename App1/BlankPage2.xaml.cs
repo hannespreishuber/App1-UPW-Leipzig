7e3454e71ad7e0a9bfba705e78c324c014a058fe
bill gates <hannesp@ppedv.de>
2017-09-25 14:48:18 +0200

    Projektdateien hinzufügen.

---
+++ App1/BlankPage2.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Web.Syndication;
+
+// Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=234238 dokumentiert.
+
+namespace App1
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class BlankPage2 : Page
+    {
+        public BlankPage2()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_ClickAsync(object sender, RoutedEventArgs e)
+        {
+
+            var client = new SyndicationClient();
+            Uri feedUri = new Uri("https://www.langweiledich.net/feed/");
+            SyndicationFeed feed = await client.RetrieveFeedAsync(feedUri);
+            rssOutput.Text = feed.Title.Text + Environment.NewLine;
+            foreach (SyndicationItem item in feed.Items)
+            {
+                rssOutput.Text += item.Title.Text + ", " + item.PublishedDate.ToString() + Environment.NewLine;
+            }
+        }
+
+
+    }
+}
+
