b1681b2a5b217689d5fd14532a588462a0af8de5
bill gates <hannesp@ppedv.de>
2017-09-27 14:25:54 +0200

    ObservableCollection

---
+++ App1/BlankPage18.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=234238 dokumentiert.
+
+namespace App1
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class BlankPage18 : Page
+    {
+        public BlankPage18()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            LayoutRoot.DataContext = new KundenVM();
+            var file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///northwind.json"));
+            string result = await FileIO.ReadTextAsync(file);
+
+            var liste=JsonConvert.DeserializeObject<List<Customer>>(result);
+            foreach (var item in liste)
+            {
+                ((KundenVM)LayoutRoot.DataContext).Customers.Add(item);
+            }
+          
+        }
+    }
+}
---
+++ App1/KundenVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace App1
+{
+    class KundenVM
+    {
+        public ObservableCollection<Customer> Customers { get; set; } = new ObservableCollection<Customer>();
+        public KundenVM()
+        {
+       
+       
+        }
+    }
+}
